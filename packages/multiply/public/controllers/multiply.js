2aec43596d785855ab4c0ee004518b621a11a0e2
fengchao <fengchao_ict@163.com>
2014-09-13 01:36:38 +0800

    finish multiply

--- packages/multiply/public/controllers/multiply.js
+++ packages/multiply/public/controllers/multiply.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('mean.multiply').controller('MultiplyController', ['$scope', 'Global', 'Multiply',
-    function($scope, Global, Multiply) {
+angular.module('mean.multiply').controller('MultiplyController', ['$scope', '$timeout', 'Global', 'Multiply',
+    function($scope, $timeout, Global, Multiply) {
         $scope.global = Global;
         $scope.package = {
             name: 'multiply'
@@ -9,29 +9,62 @@ angular.module('mean.multiply').controller('MultiplyController', ['$scope', 'Glo
 
         $scope.phase = 0;
 
-        $scope.quesSet = [{
-            'first' : 23,
-            'second' : 67,
-            'answer' : [100,200,300,400]
-        },{
-            'first' : 23,
-            'second' : 67,
-            'answer' : [100,200,300,400]
-        }];
-
-        $scope.cur = 0;
+        $scope.cur = {};
+        $scope.iter = 0;
+        $scope.result = 0;
+        $scope.last = -1;
+        //parameter
+        $scope.time = 10;
+        $scope.firstmin = 100;
+        $scope.firstmax = 999;
+        $scope.secondmin = 10;
+        $scope.secondmax = 99;
 
         $scope.start = function(){
             $scope.phase = 1;
-            $scope.cur = 0;
+            $scope.cur = randMultiply();
+            $scope.iter = 0;
+
+            $timeout(finish,1000 * $scope.time);
         };
 
-        $scope.next = function(){
-          if($scope.cur < $scope.quesSet.length - 1){
-              $scope.cur = $scope.cur + 1;
-          }else{
-              $scope.phase = 2;
-          }
+        $scope.next = function(ans){
+          $scope.result += judge(ans);
+          $scope.iter = $scope.iter + 1;
+          $scope.cur = randMultiply();
         };
+
+        function finish(){
+            $scope.phase = 2;
+        }
+
+        function judge(ans){
+            var realVal = $scope.cur.first * $scope.cur.second;
+
+            if($scope.cur.answer[ans] === realVal){
+                $scope.last = 1;
+                return 1;
+            }else{
+                $scope.last = 0;
+                return 0;
+            }
+        }
+
+        function randMultiply(){
+            var three = Math.floor(Math.random() * ($scope.firstmax - $scope.firstmin) + $scope.firstmin);
+            var two = Math.floor(Math.random() * ($scope.secondmax - $scope.secondmin) + $scope.secondmin);
+            var answer = three * two;
+            var digit = (parseInt(answer / 10) + 1) % 10;
+            var fakeAnswer = parseInt((answer % 10) + (digit * 10) + (parseInt(answer / 100) * 100));
+            var index = Math.random() > 0.5 ? 0 : 1;
+            var answers = [];
+            answers[index] = answer;
+            answers[1 - index] = fakeAnswer;
+            return {
+                'first' : three,
+                'second' : two,
+                'answer' : answers
+            };
+        }
     }
 ]);
