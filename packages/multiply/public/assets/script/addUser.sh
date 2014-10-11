4ded6934ac323492d6be0a40c67d2311035bd710
fengchao <fengchao_ict@163.com>
2014-10-12 00:09:01 +0800

    old version register

---
+++ packages/multiply/public/assets/script/addUser.sh
@@ -0,0 +1,1 @@
+curl -XPOST http://localhost:4000/register?email=3%40163.com&password=123123123&confirmPassword=123123123&username=222&name=222
---
+++ packages/multiply/server/service/dump.js
@@ -0,0 +1,3 @@
+/**
+ * Created by fengchao on 14-10-1.
+ */
